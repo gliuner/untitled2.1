317077991fb5cf796692ce1f2a9645c4712910d9
gliuner <gliuner@mail.ru>
2022-09-08 18:15:53 +0300

    dlkfvn

--- src/ru/mirea/lab1/Main.java
+++ src/ru/mirea/lab1/Main.java
@@ -3,7 +3,7 @@ package ru.mirea.lab1;
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book ();
-        book1.setTitle("My Name.dikngv..");
+        book1.setTitle("My Name...");
         System.out.println();
         book1.readBook();
     }
